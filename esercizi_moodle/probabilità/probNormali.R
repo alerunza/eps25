fa00ec6353f8c7c1df880a9efebb1f8e37750e42
alerunza <alessandrorunza@icloud.com>
2026-01-06 17:19:23 +0100

    norm + 1

--- esercizi_moodle/probabilità/probNormali.R
+++ esercizi_moodle/probabilità/probNormali.R
@@ -146,3 +146,28 @@ round(pnorm(2.5, 0, 2) + (1 - pnorm(3, 0, 2)), 4)
 (1 - pnorm(1.7 / 2, 0, 1)) / (1 - pnorm(1 / 2, 0, 1))
 round((1 - pnorm(1.7 / 2, 0, 1)) / (1 - pnorm(1 / 2, 0, 1)), 4)
 [1] 0.6406
+
+# Sia X una variabile aleatoria distribuita come una Normale di media 0 e varianza 4.
+# Determinare:
+
+# 1 la probabilità che X sia minore di 1.5
+# 2 la probabilità che X sia maggiore di 3.
+# 3 la probabilità che X sia maggiore di 3 sapendo che X è minore di 6.
+
+# soluzione:
+
+#1
+pnorm(1.5, 0, sqrt(4))
+round(pnorm(1.5, 0, sqrt(4)), 4)
+[1] 0.7734
+
+#2
+1 - pnorm(3, 0, sqrt(4))
+round(1 - pnorm(3, 0, sqrt(4)), 4)
+[1] 0.0668
+
+#3 probabilità condizionata P(A|B) = P(A ∩ B) / P(B)
+(pnorm(6, 0, sqrt(4)) - pnorm(3, 0, sqrt(4))) / pnorm(6, 0, sqrt(4))
+round((pnorm(6, 0, sqrt(4)) - pnorm(3, 0, sqrt(4))) / pnorm(6, 0, sqrt(4)), 4)
+[1] 0.0655
+
