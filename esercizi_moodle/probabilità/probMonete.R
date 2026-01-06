1ef280afafed3ab55c956c3e7e5d2ae08b679de6
alerunza <alessandrorunza@icloud.com>
2026-01-06 11:41:47 +0100

    monete (non equa)

--- esercizi_moodle/probabilità/probMonete.R
+++ esercizi_moodle/probabilità/probMonete.R
@@ -1,9 +1,7 @@
 #Si tira quattro volte una moneta EQUA. Calcolare:
 
 #1 la probabilità dell'evento A: "I primi tre risultati sono uguali":
-
 #2 la probabilità dell'evento B: "Gli ultimi due risultati sono uguali":
-
 #3 P(A ∩ B):
 
 #soluzione:
@@ -27,3 +25,33 @@ round(p_B, 4)
 p_A_and_B <- 2 * (1/2)^4
 round(p_A_and_B, 4)
 [1] 0.125
+
+# Si tira quattro volte una moneta non equa, con probabilità di ottenere testa pari a 1/3. 
+# Calcolare:
+
+#1 la probabilità dell'evento A: "I primi tre risultati sono uguali":
+#2 la probabilità dell'evento B: "Gli ultimi due risultati sono uguali":
+#3 P(A ∩ B):
+
+#soluzione:
+
+#1
+# i primi tre risultati sono uguali: possono essere tutti teste o tutti croce
+p_A <- (1/3)^3 + (2/3)^3
+round(p_A, 4)
+[1] 0.3333
+
+#2
+# gli ultimi due risultati sono uguali: possono essere tutti teste o tutti croce
+p_B <- (1/3)^2 + (2/3)^2
+round(p_B, 4)
+[1] 0.5556
+
+#3
+# i primi tre risultati sono uguali e gli ultimi due sono uguali: i primi tre
+# possono essere tutti teste o tutti croce, e l'ultimo risultato deve essere uguale
+# al terzo
+p_A_and_B <- (1/3)^4 + (2/3)^4
+round(p_A_and_B, 4)
+[1] 0.2099
+
