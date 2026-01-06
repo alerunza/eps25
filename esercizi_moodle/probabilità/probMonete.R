1df2062ef3915266a5add0b834c4249d7082235b
Giorgia Sulis <giorgiasulis1@gmail.com>
2026-01-06 12:29:01 +0100

    Add probability exercise for three coin launches
    Added a new exercise regarding probability with three coins, detailing the launch procedure and calculating probabilities for specific sequences.

--- esercizi_moodle/probabilità/probMonete.R
+++ esercizi_moodle/probabilità/probMonete.R
@@ -55,3 +55,19 @@ p_A_and_B <- (1/3)^4 + (2/3)^4
 round(p_A_and_B, 4)
 [1] 0.2099
 
+— Esercizio —
+
+| Si hanno a disposizione 3 monete. La moneta A ha probabilità di ottenere testa pari a  p . La moneta B ha probabilità di ottenere testa pari a  p/2 . La moneta C ha probabilità di ottenere testa pari a  p/3 . Si fanno tre lanci consecutivi con la seguente procedura:
+
+- Primo lancio: Si lancia la moneta A.
+- Secondo lancio: Se al lancio precedente si è ottenuto testa allora si lancia la moneta A. Se si è avuto croce si lancia la moneta B.
+- Terzo lancio: Se nel lancio precedente si è ottenuto testa allora si lancia la moneta A. Se nel primo lancio si è ottenuto testa e nel secondo lancio si è ottenuto croce, allora si lancia la moneta B. Se nel primo e nel secondo lancio si è ottenuto croce allora si lancia la moneta C.
+
+1. Se  p = 1/4 , determinare la probabilità della sequenza (Testa,Testa,Testa):
+(1/4)^3 = 0.0156
+
+2. Se  p = 1/2 , determinare la probabilità della sequenza (Croce, Croce, Croce):
+poichè qui si tratta di croce, prendo le probabilità di ciauscuna moneta (calcolata per testa) e poi prendo tutto il resto (per croce)
+e si moltiplica tutto --> 1/2 * 3/4 * 5/6 = 0.3125
+
+3. Per quale valore di  p  la sequenza (Croce, Croce, Croce) ha probabilità zero?
