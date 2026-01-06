4c78aa7629d1f8c76f3d00e7105da80e636a13c3
alerunza <alessandrorunza@icloud.com>
2026-01-06 11:24:01 +0100

    exp + 1

--- esercizi_moodle/probabilità/probExp.R
+++ esercizi_moodle/probabilità/probExp.R
@@ -2,9 +2,7 @@
 # Determinare:
 
 # 1 La probabilità che X sia minore di 1.5.
-
 # 2 La probabilità che X sia maggiore di 3.
-
 # 3 La probabilità che X sia maggiore di 3 sapendo che X è maggiore di 6.
 
 # NB: può essere utile la funzione pexp()
@@ -26,3 +24,29 @@ round(1 - pexp(3, 1), 4)
 (pexp(6, 1) - pexp(3, 1)) / pexp(6, 1)
 round((pexp(6, 1) - pexp(3, 1)) / pexp(6, 1), 4)
 [1] 0.0474
+
+# Sia X una variabile aleatoria distribuita come una Esponenziale di parametro 2.
+# Determinare:
+
+# 1 La probabilità che X sia minore di 1.5 oopure maggiore di 3.
+# 2 La probabilità che X sia maggiore di 3.
+# 3 La probabilità che "X appartenga all'intervallo (1,6) e sia maggiore di 3"  
+
+#NB : può essere utile la funzione pexp()
+
+# soluzione:
+
+#1
+pexp(1.5, 2) + (1 - pexp(3, 2))
+round(pexp(1.5, 2) + (1 - pexp(3, 2)), 4)
+[1] 0.9527
+
+#2
+1 - pexp(3, 2)
+round(1 - pexp(3, 2), 4)
+[1] 0.0025
+
+#3 probabilità congiunta P(A ∩ B) = P(B) dato che B è sottoinsieme di A
+(pexp(6, 2) - pexp(3, 2))
+round((pexp(6, 2) - pexp(3, 2)), 4)
+[1] 0.0025
