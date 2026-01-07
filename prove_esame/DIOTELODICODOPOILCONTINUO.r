889417b768197e83e4e979b8b981a0d49e48a373
alerunza <alessandrorunza@icloud.com>
2026-01-07 19:31:45 +0100

    (to do prova esame)

---
+++ prove_esame/DIOTELODICODOPOILCONTINUO.r
@@ -0,0 +1,19 @@
+###PROBABILITÀ###
+#1
+pnorm(1.5, 0, 2)
+
+#2
+1 - pnorm(3, 0, 2)
+
+#3
+pnorm(1, 0, 2) - pnorm(-2, 0 ,2)
+
+#4
+#probabilità condizionata
+(pnorm(6, 0, 2) - pnorm(3, 0, 2)) / pnorm(6, 0, 2)
+
+
+###STATISTICA###
+#atleti.RData
+
+load("atleti.RData")
