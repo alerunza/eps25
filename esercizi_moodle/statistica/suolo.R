d40613f7267467d2a5d42c696b56ee1dc8dea8bd
alerunza <alessandrorunza@icloud.com>
2026-01-08 10:48:22 +0100

    suolo

--- esercizi_moodle/statistica/suolo.R
+++ esercizi_moodle/statistica/suolo.R
@@ -20,3 +20,37 @@ head(dati)
 names(dati)
 [1] "suolo"  "pianta"
 
+nrow(dati)
+[1] 203
+
+which(is.na(dati), arr.ind = TRUE)
+    row col
+128 128   1
+
+freq <- sum(dati$suolo == "Argilloso", na.rm = TRUE)
+freq_rel <- freq / sum(!is.na(dati$suolo))
+round(freq_rel, 4)
+[1] 0.2624
+
+sum(dati$suolo[dati$pianta=="Presente"] == "Argilloso", na.rm = TRUE)
+[1] 26
+
+barplot(table(dati$suolo))
+
+test <- chisq.test(table(dati$suolo, dati$pianta))
+
+round(test$p.value, 4)
+[1] 0.192
+
+# se p-value < 0.05 allora rifiutiamo H0
+# se p-value >= 0.05 allora non rifiutiamo H0
+# quindi non rifiutiamo H0 al livello di significatività 0.05
+# e quindi le due variabili sono indipendenti (domanda 7)
+
+#cochran -> la più grande (MAX) frequenza attesa
+max(test$expected)
+[1] 45.36139
+
+# se la più grande frequenza attesa è >= 5 allora
+# l'approssimazione del test del chi quadro è valida
+# e viene rispettata la reogla di Cochran (domanda 8)
