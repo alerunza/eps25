9b26125dc8c05f4f4ca540cbde4d2845089b6e93
Giorgia Sulis <giorgiasulis1@gmail.com>
2026-01-09 20:27:34 +0100

    Add correlation test and linear regression analysis

--- esercizi_moodle/statistica/fito.R
+++ esercizi_moodle/statistica/fito.R
@@ -9,13 +9,17 @@ quantile(fito$temperatura, 0.05, na.rm=TRUE)
 #scatteplot = comando plot
 plot(fito$temperatura, fito$fitoplancton)
 
+#
 cor.test(fito$temperatura, fito$fitoplancton)
 
 test<-cor.test(fito$temperatura, fito$fitoplancton)
+round(test$estimate, 4) #correlazione campionaria (o leggere "cor")
 round(test$p.value, 4)
 
+#regressione lineare
 mod<-lm(fito$temperatura ~ fito$fitoplancton)
 round(coef(mod), 4)
 
+#R^2
 sum<-summary(mod)
 sum$r.squared
