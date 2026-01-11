e826d833fd2e8943c7ef2b6acbdf8d67f567e1ac
alerunza <alessandrorunza@icloud.com>
2026-01-11 11:41:16 +0100

    domanda 106

--- esercizi_moodle/probabilità/probPallineMon.R
+++ esercizi_moodle/probabilità/probPallineMon.R
@@ -1,3 +1,4 @@
+#Domanda 111
 #L’urna A contiene 10 palline bianche e 10 nere. L’urna B contiene 5 palline bianche e 15 nere. 
 #Viene lanciata una moneta non truccata: se esce testa allora vengono fatte 3 estrazioni senza reinserimento dall’urna A; 
 #se esce croce vengono fatte 5 estrazioni con reinserimento dall’urna B.
@@ -33,6 +34,7 @@ opzione 2: choose(5,2)*(1/4)^2*(3/4)^3
 
 #----------------------------------------------------------------------------#
 
+#Domanda 104
 #Procedi con il seguente esperimento: lanci una moneta truccata che restituisce testa con probabilità 0.3. 
 #Se ottieni testa estrai con reimbussolamento 8 palline da un’urna che ne contiene 11, delle quali 4 sono bianche e 7 sono verdi. 
 #Se ottieni croce estrai, con reimbussolamento dalla stessa urna, 6 palline.
@@ -101,6 +103,7 @@ den <- num +
 num / den
 #----------------------------------------------------------------------------#
 
+#Domanda 105
 #Procedi con il seguente esperimento: lanci una moneta truccata che restituisce testa con probabilità 0.2. 
 #Se ottieni testa estrai con reimbussolamento 8 palline da un’urna che ne contiene 12, delle quali 5 sono bianche e 7 sono verdi. 
 #Se ottieni croce estrai, con reimbussolamento dalla stessa urna, 6 palline.
@@ -135,3 +138,54 @@ den <- num +
 num / den
 #----------------------------------------------------------------------------#
 
+#Domanda 106
+#Procedi con il seguente esperimento: lanci una moneta truccata che restituisce testa con probabilità 0.4. 
+#Se ottieni testa estrai con reimbussolamento 8 palline da un’urna che ne contiene 11, delle quali 2 sono bianche e 9 sono verdi. 
+#Se ottieni croce estrai, con reimbussolamento dalla stessa urna, 6 palline.
+
+# 1 Con quale probabilità ottieni 3 palline verdi?
+# 2 Con quale probabilità ottieni 7 palline verdi?
+# 3 Con quale probabilità ottieni almeno (>=) 3 palline verdi?
+# 4 Con quale probabilità avevi ottenuto testa se hai estratto meno di () 3 palline verdi?
+
+# soluzione:
+
+# 1
+dbinom(3, 8, 9/11) * 0.4 + dbinom(3, 6, 9/11) * 0.6
+
+#2
+dbinom(7, 8, 9/11) * 0.4 + dbinom(7, 6, 9/11) * 0.6
+
+#3
+#dato che le estrazioni le facciamo da 0 a 8 o 6, calcoliamo il complemento:
+# P(X >= 3) = 1 - P(X < 3)
+1 - (dbinom(0, 8, 9/11) * 0.4 + dbinom(1, 8, 9/11) * 0.4 + dbinom(2, 8, 9/11) * 0.4 +
+dbinom(0, 6, 9/11) * 0.6 + dbinom(1, 6, 9/11) * 0.6 + dbinom(2, 6, 9/11) * 0.6)
+
+# oppure direttamente sommando le probabilità:
+# di base le estrazioni sono da 0 a 8 o 6, quindi sommo da 3 a 8 o 6
+# almeno 3 verdi = 3,4,5,6,7,8 per 8 estrazioni
+dbinom(3, 8, 9/11) * 0.4 +
+dbinom(4, 8, 9/11) * 0.4 +
+dbinom(5, 8, 9/11) * 0.4 +
+dbinom(6, 8, 9/11) * 0.4 +
+dbinom(7, 8, 9/11) * 0.4 +
+dbinom(8, 8, 9/11) * 0.4 +
+# 3,4,5,6 per 6 estrazioni
+dbinom(3, 6, 9/11) * 0.6 +
+dbinom(4, 6, 9/11) * 0.6 +
+dbinom(5, 6, 9/11) * 0.6 +
+dbinom(6, 6, 9/11) * 0.6
+
+#4
+num <- (dbinom(0, 8, 9/11) +
+        dbinom(1, 8, 9/11) +
+        dbinom(2, 8, 9/11)) * 0.4
+
+den <- num +
+       (dbinom(0, 6, 9/11) +
+        dbinom(1, 6, 9/11) +
+        dbinom(2, 6, 9/11)) * 0.6
+
+num / den
+#----------------------------------------------------------------------------#
